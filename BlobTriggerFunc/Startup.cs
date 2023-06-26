2a96562eaae1dfa8186ff090daa57e8bfa0a5ddc
Gary <gbrown31@gmail.com>
2023-06-26 21:21:15 +0100

    add azurite blob triggerd func.

---
+++ BlobTriggerFunc/Startup.cs
@@ -0,0 +1,25 @@
+﻿using BlobTriggerFunc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+[assembly: WebJobsStartup(typeof(Startup))]
+namespace BlobTriggerFunc
+{
+    public class Startup : IWebJobsStartup
+    {
+        public void Configure(IWebJobsBuilder builder)
+        {
+            //builder.Services.
+            var config = new ConfigurationBuilder()
+               .SetBasePath(Environment.CurrentDirectory)
+               .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+               .AddEnvironmentVariables()
+               .Build();
+
+            builder.Services.AddSingleton<IConfiguration>(config);
+        }
+    }
+}
